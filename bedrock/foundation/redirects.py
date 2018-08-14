b3da79d764ba8bf1fd625f71e624aedf730fd5c6
Amy Churchwell <achurchwell@mozilla.com>
2018-08-14 12:54:34 -0700

    Create redirects in /foundation.

---
+++ bedrock/foundation/redirects.py
@@ -0,0 +1,10 @@
+from bedrock.redirects.util import redirect
+
+redirectpatterns = [
+# issue #5949
+    redirect(r'^foundation/trademarks/$', '/foundation/trademarks/policy/'),
+    redirect(r'^foundation/trademarks/faq/$', '/foundation/trademarks/policy/'),
+    redirect(r'^foundation/documents/domain-name-license.pdf$', '/foundation/trademarks/policy/'),
+    redirect(r'^about/partnerships/distribution/$', '/foundation/trademarks/distribution-policy/'),
+    redirect(r'^foundation/trademarks/poweredby/faq/$', '/foundation/trademarks/policy/')
+]
